2433eb28383156cf10404c5b2701dbbf939d21ca
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-21 21:21:38 +0200

    Add errors handling for server

--- server/server.go
+++ server/server.go
@@ -2,14 +2,21 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func Server(addr string) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprintf("Response from :%v\n", addr)))
+		_, err := w.Write([]byte(fmt.Sprintf("Response from :%v\n", addr)))
+		if err != nil {
+			log.Printf("Failed to write response on %s: %v", addr, err)
+		}
 	})
 
-	http.ListenAndServe(addr, mux)
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatalf("Failed to start server on %s: %v", addr, err)
+	}
 }
