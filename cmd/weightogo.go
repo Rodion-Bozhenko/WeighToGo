a64adb8e127f8f34a58048452f1d29a68b42adf5
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-24 23:21:39 +0200

    Implement prototype of health check

--- cmd/weightogo.go
+++ cmd/weightogo.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
+	"time"
+	"weightogo/healthcheck"
 	"weightogo/loadbalancer"
 	"weightogo/logger"
 )
+
+var servers = []loadbalancer.Server{
+	{Address: "http://localhost:7230"},
+	{Address: "http://localhost:9001"},
+	{Address: "http://localhost:8002"},
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8080")
@@ -13,12 +22,7 @@ func main() {
 		logger.Logger.Error("Error setting up listener", "err", err.Error())
 		listener.Close()
 	}
-
-	servers := []loadbalancer.Server{
-		{Address: "localhost:5000", Weight: 1},
-		{Address: "localhost:5001", Weight: 5},
-		{Address: "localhost:5002", Weight: 1},
-	}
+	defer listener.Close()
 
 	host, port, err := net.SplitHostPort(listener.Addr().String())
 	if err != nil {
@@ -27,8 +31,45 @@ func main() {
 
 	logger.Logger.Info(fmt.Sprintf("Listening on host: %v, port: %v", host, port))
 
-	lb := loadbalancer.GetLoadBalancer(loadbalancer.LeastConnections, servers)
+	aliveServers := getHealthyServers(servers)
+
+	if len(aliveServers) == 0 {
+		logger.Logger.Error("No available servers. Health check failed on each server.")
+		return
+	}
+	lb := loadbalancer.GetLoadBalancer(loadbalancer.LeastConnections, aliveServers)
 	connectionHandler := NewConnectionHandler(listener, lb, logger.Logger)
 
 	connectionHandler.HandleConnection()
+}
+
+func getHealthyServers(servers []loadbalancer.Server) []loadbalancer.Server {
+	ch := make(chan loadbalancer.Server, len(servers))
+	var wg sync.WaitGroup
+
+	for _, s := range servers {
+		wg.Add(1)
+		go func(s loadbalancer.Server) {
+			defer wg.Done()
+			alive, err := healthcheck.IsAlive(s.Address, time.Second*5)
+			if err != nil {
+				logger.Logger.Error(fmt.Sprintf("Unable to healthcheck server %s", s.Address), "err", err)
+			}
+			if alive {
+				ch <- s
+			}
+		}(s)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	var aliveServers []loadbalancer.Server
+	for s := range ch {
+		aliveServers = append(aliveServers, s)
+	}
+
+	return aliveServers
 }
